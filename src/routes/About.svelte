bd70bac4c07eb2caa9b7022da5d2c143d2db6c38
Jedemekong <jedemekong@Jed-MBA.local>
2025-01-03 10:27:43 +0100

    changes to about page

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -10,19 +10,30 @@
   <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
     <div>
       <p class="mb-4 text-gruvbox-fg4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        ℹ️ Hi, I'm Jed, a Software Engineering student at <a href="https://topfaith.edu.ng/"><u>Topfaith University</u></a> in Nigeria. 
+        I'm passionate about technology and love immersing myself in roguelike games 
+        like Dead Cells, Hades, and Blasphemous.
       </p>
       <p class="mb-4 text-gruvbox-fg4">
-        [Your experience] Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-        laboris nisi ut aliquip ex ea commodo consequat.
+        📅 I’ve been programming for over 5 years, starting in my high school days (SS2). 
+        My journey spans diverse areas of computer science, from e-commerce and enterprise 
+        software to even game development.
+      </p>
+      <p class="mb-4 text-gruvbox-fg4">
+        👷🏽‍♂️ I’m currently building blockchain products on <a href="https://solana.com/"><u>Solana</u></a> and exploring the power of AI. 
+        I’m a fan of Rust and enjoy building with it. I live in the terminal (Kitty/Zellij/Neovim) and 
+        I am a strong advocate for developer efficiency.
       </p>
       <h2 class="text-2xl font-bold mt-8 mb-4 text-gruvbox-fg heading-depth">Skills</h2>
       <ul class="list-disc list-inside space-y-2 text-gruvbox-fg4">
         <li>JavaScript/TypeScript</li>
-        <li>React/Svelte</li>
+        <li>Svelte/SvelteKit</li>
         <li>Node.js</li>
-        <li>HTML/CSS</li>
+        <li>Rust</li>
+        <li>Python</li>
+        <li>Lua</li>
+        <li>Bash</li>
+        <li>Nix</li>
       </ul>
     </div>
     <div>
